5808587ec459fa03abe7cba577cf5769eeba2380
Turik59 <gafuryanov.a@mail.ru>
2024-05-16 14:38:24 +0500

    Mazitov Gafuryanov - TETRIS

---
+++ Tetris/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Tetris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(428, 220);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.75F);
+            this.label2.Location = new System.Drawing.Point(428, 266);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(649, 641);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Tetris";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.OnPaint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tetris/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tetris.Controllers;
+
+namespace Tetris
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer sp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyUp += new KeyEventHandler(keyFunc);
+            sp = new SoundPlayer(@"tetris_music.wav");
+            sp.Play();
+            Init();
+        }
+
+        public void Init()
+        {
+            MapController.size = 25;
+            MapController.score = 0;
+            MapController.linesRemoved = 0;
+            MapController.currentShape = new Shape(3, 0);
+            MapController.Interval = 300;
+            label1.Text = "Score: " + MapController.score;
+            label2.Text = "Lines: " + MapController.linesRemoved;
+
+            timer1.Interval = MapController.Interval;
+            timer1.Tick += new EventHandler(update);
+            timer1.Start();
+            
+            Invalidate();
+        }
+
+        private void keyFunc(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+
+                    if (!MapController.IsIntersects())
+                    {
+                        MapController.ResetArea();
+                        MapController.currentShape.RotateShape();
+                        MapController.Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.Down:
+                    timer1.Interval = 10;
+                    break;
+                case Keys.Right:
+                    if (!MapController.CollideHor(1))
+                    {
+                        MapController.ResetArea();
+                        MapController.currentShape.MoveRight();
+                        MapController.Merge();
+                        Invalidate();
+                    }
+                    break;
+                case Keys.Left:
+                    if (!MapController.CollideHor(-1))
+                    {
+                        MapController.ResetArea();
+                        MapController.currentShape.MoveLeft();
+                        MapController.Merge();
+                        Invalidate();
+                    }
+                    break;
+            }
+        }
+
+        
+        private void update(object sender, EventArgs e)
+        {
+            MapController.ResetArea();
+            if (!MapController.Collide())
+            {
+                MapController.currentShape.MoveDown();
+            }
+            else
+            {
+                MapController.Merge();
+                MapController.SliceMap(label1,label2);
+                timer1.Interval = MapController.Interval;
+                MapController.currentShape.ResetShape(3,0);
+                if (MapController.Collide())
+                {
+                    MapController.ClearMap();
+                    timer1.Tick -= new EventHandler(update);
+                    timer1.Stop();
+                    MessageBox.Show("Result: " + MapController.score);
+                    Init();
+                }
+            }
+            MapController.Merge();
+            Invalidate();
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            MapController.DrawGrid(e.Graphics);
+            MapController.DrawMap(e.Graphics);
+            MapController.ShowNextShape(e.Graphics);
+        }
+    }
+}
